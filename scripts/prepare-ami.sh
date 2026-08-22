d8df5629c9bb77609c3d116fcfe00c9f249a8e30
Tahir-Alakbarli <tacin.namazov@gmail.com>
2026-08-23 00:18:34 +0200

    Update prepare-ami.sh

--- scripts/prepare-ami.sh
+++ scripts/prepare-ami.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 BUCKET_NAME="tahir-image-frontend"
 
 dnf install -y httpd awscli
-systemctl enable http
+systemctl enable httpd
 aws s3 sync "s3://${BUCKET_NAME}/" /var/www/html/ --delete
 chown -R apache:apache /var/www/html
 printf '%s\n' 'ErrorDocument 404 /404.html' > /etc/httpd/conf.d/site-errors.conf
